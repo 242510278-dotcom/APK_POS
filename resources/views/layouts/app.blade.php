e810861753d4e9ae61ea2dfe886e1f7525db76ea
242510278-dotcom <242510278@smkn4-tsm.sch.id>
2026-08-07 14:23:34 +0700

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,25 +2,26 @@
 <html lang="id">
 <head>
     <meta charset="UTF-8">
-    <!-- Isi title yang kita kirimkan dari views lain-->
-    <title>@yield('title')</title>
-    <!-- memanggil Link bootstraps-->
-    @vite(['resources/css/app.css', 'resources/js/app.js'])   
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title', 'Aplikasi Penjualan')</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
 
-    <div class="container">
+    {{-- 1. PEMANGGILAN NAVBAR DI SINI --}}
+
+
+    <div class="container mt-4">
 
         @if(session('success'))
-        <div class="alert alert-success">
-            {{ session('success') }}
-</div>
-@endif
-      <!-- Isi konten yang kita kirimkan dari views lain-->
-      @yield('content')   
-            
- </div>
-    
-        
+            <div class="alert alert-success">
+                {{ session('success') }}
+            </div>
+        @endif
+
+        @yield('content')
+
+    </div>
+
 </body>
 </html>
