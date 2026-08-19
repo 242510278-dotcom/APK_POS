d62228dbf310c934a8a38f129590be852273b568
242510278-dotcom <242510278@smkn4-tsm.sch.id>
2026-08-19 09:19:58 +0700

    Update navbar.blade.php

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,6 +1,6 @@
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
+<nav class="navbar navbar-expand-lg bg-dark shadow-sm" data-bs-theme="dark">
   <div class="container-fluid">
-    <a class="navbar-brand" href="{{ route('dashboard') }}">POS</a>
+    <a class="navbar-brand fw-bold" href="{{ route('dashboard') }}">POS</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -23,7 +23,7 @@
 
       <form class="ms-auto" action="{{ route('logout') }}" method="POST">
         @csrf
-        <button type="submit" class="btn btn-danger">Logout</button>
+        <button type="submit" class="btn btn-danger btn-sm">Logout</button>
       </form>
     </div>
   </div>
