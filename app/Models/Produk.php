a50cb523798d4bb5534b531980cca07cd32109a3
242510278-dotcom <242510278@smkn4-tsm.sch.id>
2026-09-11 13:00:20 +0700

    Update Produk.php

--- app/Models/Produk.php
+++ app/Models/Produk.php
@@ -2,28 +2,31 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
 class Produk extends Model
 {
-    use HasFactory;
-    protected $table ='produk';
+    use HasFactory, SoftDeletes;
+
+    protected $table = 'produk';
+
     protected $fillable = [
         'user_id',
         'foto',
         'nama',
         'harga_beli',
         'harga_jual',
-        'stok'
-
+        'stok',
     ];
 
     public function itemPenjualan()
     {
-        return $this->hasMany(itemPenjualan::class,'produk_id');
+        return $this->hasMany(ItemPenjualan::class, 'produk_id');
     }
 
-     public function user()
+    public function user()
     {
         return $this->belongsTo(User::class, 'user_id');
     }
